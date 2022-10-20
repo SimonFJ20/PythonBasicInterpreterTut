613a72474b67e87eadf8238d2ad637fb8fbcb86b
SimonFJ20 <simonfromjakobsen@gmail.com>
2022-10-21 00:34:45 +0200

    hashmaps get set

--- danskc/runtime/hashmap.h
+++ danskc/runtime/hashmap.h
@@ -2,6 +2,8 @@
 #define HASHMAP_H
 
 #include "array.h"
+#include "dstring.h"
+#include "object.h"
 #include <stdint.h>
 
 typedef struct HashMapPair {
@@ -17,5 +19,7 @@ void construct_hashmap(HashMap* hashmap);
 void destruct_hashmap(HashMap* hashmap);
 HashMap* new_hashmap();
 void delete_hashmap();
+void set(String* key, Object* value);
+void get(String* key);
 
 #endif
