64549072e7079aaca4a1c8ae1a8900fdf1a9f9a4
SimonFJ20 <simonfromjakobsen@gmail.com>
2022-10-26 23:09:37 +0200

    yee

--- danskc/runtime/src/main.rs
+++ danskc/runtime/src/main.rs
@@ -1,3 +1,15 @@
+use std::collections::HashMap;
+
+struct Object {
+    next_link_node: Option<Box<Object>>,
+    gc_marked: bool,
+    properties: HashMap<String, Object>,
+}
+
+enum Instruction {
+    Jump(usize),
+}
+
 fn main() {
     println!("Hello, world!");
 }
