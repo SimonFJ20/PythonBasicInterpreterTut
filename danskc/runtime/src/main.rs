616a2f56113741cb08b36689d6bf2ac4a7aa834e
SimonFJ20 <simonfromjakobsen@gmail.com>
2022-10-28 19:13:17 +0200

    properly

--- danskc/runtime/src/main.rs
+++ danskc/runtime/src/main.rs
@@ -1,13 +1,53 @@
 use std::collections::HashMap;
 
+enum ObjectValue {
+    Null,
+    Int(i64),
+    Float(f64),
+    String(String),
+    List(Vec<Object>),
+    Dict(HashMap<String, Object>),
+    Clojure(HashMap<String, Object>, usize),
+}
+
 struct Object {
     next_link_node: Option<Box<Object>>,
     gc_marked: bool,
-    properties: HashMap<String, Object>,
+    value: ObjectValue,
+}
+
+impl Object {
+    pub fn new(value: ObjectValue) -> Self {
+        Self {
+            gc_marked: false,
+            next_link_node: None,
+            value,
+        }
+    }
 }
 
 enum Instruction {
     Jump(usize),
+    LoadReg(usize),
+    StoreReg(usize),
+    Push(Object),
+    Pop,
+}
+
+struct VM {
+    program: Vec<Instruction>,
+    stack: Vec<Object>,
+    registers: HashMap<usize, Object>,
+}
+
+impl VM {
+    pub fn new(program: Vec<Instruction>) -> Self {
+        Self {
+            program,
+            stack: Vec::new(),
+            registers: HashMap::new(),
+        }
+    }
 }
 
 fn main() {
